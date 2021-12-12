9f33e86448847af2aa72e9ba9ba6d0484d780a6c
Leland Hayes <lelandkhayes13@gmail.com>
2021-12-12 07:40:14 -0600

    fixed some spacing on contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,102 +1,146 @@
 import React from "react";
 import { useForm, ValidationError } from "@formspree/react";
+// import anime from "animejs/lib/anime.es.js";
 
 const Contact = () => {
   const [state, handleSubmit] = useForm("mbjwjgga");
   if (state.succeeded) {
-    return <div className="is-flex is-justify-content-center is-align-items-center" style={{ height: "30rem"}}><div><p className="m12">Thank You for Contacting Us!</p></div></div>;
+    return (
+      <div
+        className="is-flex is-justify-content-center is-align-items-center"
+        style={{ height: "30rem" }}
+      >
+        <div>
+          <h1 className="ml2">Thank you for reaching out!</h1>
+        </div>
+      </div>
+    );
   }
+
+  // const textWrapper = document.querySelector(".ml2");
+  // textWrapper.innerHTML = textWrapper.textContent.replace(
+  //   /\S/g,
+  //   "<span class='letter'>$&</span>"
+  // );
+
+  // anime
+  //   .timeline({ loop: true })
+  //   .add({
+  //     targets: ".ml2 .letter",
+  //     scale: [4, 1],
+  //     opacity: [0, 1],
+  //     translateZ: 0,
+  //     easing: "easeOutExpo",
+  //     duration: 950,
+  //     delay: (el, i) => 70 * i,
+  //   })
+  //   .add({
+  //     targets: ".ml2",
+  //     opacity: 0,
+  //     duration: 1000,
+  //     easing: "easeOutExpo",
+  //     delay: 1000,
+  //   });
 
   return (
     <div className="is-align-items-center">
       <h2
         className="has-text-centered is-size-2"
-        style={{ 
+        style={{
           marginTop: "5rem",
+          marginBottom: "1rem",
         }}
       >
         CONTACT US{" "}
       </h2>
-      <div className="is-flex is-justify-content-center" style={{}}><div
-        className="justify-content-center"
-        style={{
-          background: "linear-gradient(to right, #972EFF , #E197FD)",
-          height: ".2rem",
-          width: "12%",
-          marginBottom: "3rem"
-        }}
-      ></div></div>
+      <div className="is-flex is-justify-content-center" style={{}}>
+        <div
+          className="justify-content-center"
+          style={{
+            background: "linear-gradient(to right, #972EFF , #E197FD)",
+            height: ".18rem",
+            width: "8%",
+            marginBottom: "1.3rem",
+          }}
+        ></div>
+      </div>
       <div className="is-flex is-justify-content-center">
-      <p
-        className="has-text-centered is-size-4"
-        style={{ width: "75%"}}
+        <p
+          className="has-text-centered is-size-4"
+          style={{
+            width: "49%",
+            marginBottom: "1.6rem",
+          }}
         >
-        Let Digifie help you with all your digital needs. There are multiple 
-        ways to contact us so choose which one is best for you. Lets 
-        start building your dream site.{" "}
-      </p>
-      </div> 
+          Let Digifie help you with all your digital needs. There are multiple
+          ways to contact us so choose which one is best for you. Lets start
+          building your dream site.{" "}
+        </p>
+      </div>
 
-      <div className="container" style={{ marginLeft: "3rem", marginRight: "3rem"}}>
-      <form onSubmit={handleSubmit}>
-        <div id="contact-form" className="">
-          <div className="field flex is-flex-shrink-1 is-flex-direction-column is-centered">
-            <label className="c-lab">Email</label>
-            <div className="control ">
-              <input
-                className="input "
-                type="email"
-                placeholder="hello@"
-                name="email"
-              />
-              <ValidationError
-                prefix="Email"
-                field="email"
-                errors={state.errors}
-              />
-            </div>
-          </div>
-
-          <div className="field flex is-flex-shrink-1 is-flex-direction-column">
-            <label className="c-lab">Message</label>
-            <div className="control">
-              <textarea
-                className="textarea"
-                name="message"
-                placeholder="Message"
-              />
-              <ValidationError
-                prefix="Message"
-                field="message"
-                errors={state.errors}
-              />
-            </div>
-          </div>
-
-          <div className="btn-style">
-            <div className="field is-grouped is-flex is-justify-content-center">
+      <div
+        className="container"
+        style={{ marginLeft: "3rem", marginRight: "3rem" }}
+      >
+        <form onSubmit={handleSubmit}>
+          <div id="contact-form" className="">
+            <div className="field flex is-flex-shrink-1 is-flex-direction-column is-centered">
+              <label className="c-lab is-flex is-justify-content-center">Email</label>
               <div className="control ">
-                <button
-                  type="submit"
-                  disabled={state.submitting}
-                  className="button is-rounded"
-                  style={{  borderColor: "#972EFF "  }}
-                >
-                  Submit
-                </button>
+                <input
+                  className="input "
+                  type="email"
+                  placeholder="hello@"
+                  name="email"
+                />
+                <ValidationError
+                  prefix="Email"
+                  field="email"
+                  errors={state.errors}
+                />
               </div>
+            </div>
+
+            <div className="field flex is-flex-shrink-1 is-flex-direction-column">
+              <label className="c-lab is-flex is-justify-content-center">Message</label>
               <div className="control">
-                <button
-                  className="button is-rounded"
-                  style={{ borderColor: "#972EFF " }}
-                >
-                  Cancel
-                </button>
+                <textarea
+                  className="textarea"
+                  name="message"
+                  placeholder="Message"
+                />
+                <ValidationError
+                  prefix="Message"
+                  field="message"
+                  errors={state.errors}
+                />
+              </div>
+            </div>
+
+            <div className="btn-style">
+              <div className="field is-grouped is-flex is-justify-content-center">
+                <div className="control ">
+                  <button
+                    type="submit"
+                    disabled={state.submitting}
+                    className="button is-rounded"
+                    style={{ borderColor: "#972EFF " }}
+                  >
+                    Submit
+                  </button>
+                </div>
+                <div className="control">
+                  <button
+                    className="button is-rounded"
+                    style={{ borderColor: "#972EFF " }}
+                  >
+                    Cancel
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
       </div>
     </div>
   );
