4df9804f75bf8adea5100742c502cead1f7881b2
Leland Hayes <lelandkhayes13@gmail.com>
2021-12-20 09:24:21 -0600

    added basic footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footerr">
+      <div className="has-text-centered footer-cont">
+        <p className="is-size-4">© 2021 | Designed & Created by Chase Nicks and Leland Hayes</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
