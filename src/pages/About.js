04ca2d4053b0fe776a2b96960049c51c777ee9e2
Leland Hayes <lelandkhayes13@gmail.com>
2021-12-21 14:37:35 -0600

    changed format of About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,75 +3,81 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h2
-        className="has-text-centered is-size-1"
-        style={{
-          marginTop: "5rem",
-          marginBottom: "1rem",
-          fontWeight: "boldest",
-        }}
-      >
-        WHAT WE DO{" "}
-      </h2>
-      <div className="is-flex is-justify-content-center" style={{}}>
-        <div
-          className="justify-content-center"
-          style={{
-            background: "linear-gradient(to right, #972EFF , #E197FD)",
-            height: ".18rem",
-            width: "5%",
-            marginBottom: "1.3rem",
-          }}
-        ></div>
-      </div>
-      <div className="is-flex is-justify-content-center">
-        <p
-          className="has-text-centered is-size-4"
-          style={{
-            maxWidth: "40rem",
-            marginBottom: "3rem",
-            fontWeight: "bold",
-          }}
-        >
-          Let Digifie help you with all your digital needs. There are multiple
-          ways to contact us, choose which one is best for you. Lets start
-          building your dream website.{" "}
-        </p>
-      </div>
-      <h2
-        className="has-text-centered is-size-1"
-        style={{
-          marginTop: "5rem",
-          marginBottom: "1rem",
-          fontWeight: "boldest",
-        }}
-      >
-        HOW WE WORK{" "}
-      </h2>
-      <div className="is-flex is-justify-content-center" style={{}}>
-        <div
-          className="justify-content-center"
-          style={{
-            background: "linear-gradient(to right, #972EFF , #E197FD)",
-            height: ".18rem",
-            width: "5%",
-            marginBottom: "1.3rem",
-          }}
-        ></div>
-      </div>
-      <div className="is-flex is-justify-content-center">
-        <p
-          className="has-text-centered is-size-4"
-          style={{
-            maxWidth: "40rem",
-            marginBottom: "3rem",
-            fontWeight: "bold",
-          }}
-        >
-          Let Digifie help you with all your digital needs. There are multiple
-          ways to contact us, choose which one is best for you. Lets start
-          building your dream website.{" "}
-        </p>
+      <div className="columns">
+        <div className="column is-half is-flex is-flex-direction-column">
+          <h2
+            className="has-text-centered is-size-1"
+            style={{
+              marginTop: "2rem",
+              marginBottom: "1rem",
+              fontWeight: "boldest",
+            }}
+          >
+            WHAT WE DO{" "}
+          </h2>
+          <div className="is-flex is-justify-content-center" style={{}}>
+            <div
+              className="justify-content-center"
+              style={{
+                background: "linear-gradient(to right, #972EFF , #E197FD)",
+                height: ".18rem",
+                width: "6.7%",
+                marginBottom: "1rem",
+              }}
+            ></div>
+          </div>
+          <div className="is-flex is-justify-content-center">
+            <p
+              className="has-text-centered is-size-4"
+              style={{
+                maxWidth: "40rem",
+                marginBottom: "3rem",
+                fontWeight: "bold",
+              }}
+            >
+              Let Digifie help you with all your digital needs. There are
+              multiple ways to contact us, choose which one is best for you.
+              Lets start building your dream website.{" "}
+            </p>
+          </div>
+        </div>
+        <div className="column is-half is-flex is-flex-direction-column">
+          <h2
+            className="has-text-centered is-size-1"
+            style={{
+              marginTop: "2rem",
+              marginBottom: "1rem",
+              fontWeight: "boldest",
+            }}
+          >
+            HOW WE WORK{" "}
+          </h2>
+          <div className="is-flex is-justify-content-center" style={{}}>
+            <div
+              className="justify-content-center"
+              style={{
+                background: "linear-gradient(to right, #972EFF , #E197FD)",
+                height: ".18rem",
+                width: "6.7%",
+                marginBottom: "1rem",
+              }}
+            ></div>
+          </div>
+          <div className="is-flex is-justify-content-center">
+            <p
+              className="has-text-centered is-size-4"
+              style={{
+                maxWidth: "40rem",
+                marginBottom: "5rem",
+                fontWeight: "bold",
+              }}
+            >
+              Let Digifie help you with all your digital needs. There are
+              multiple ways to contact us, choose which one is best for you.
+              Lets start building your dream website.{" "}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
