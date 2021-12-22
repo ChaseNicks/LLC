9c5e342d7d545b49fa0e9e8655c96617bc18623b
Leland Hayes <lelandkhayes13@gmail.com>
2021-12-22 16:42:10 -0600

    fixed spacing on about page in values section

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,79 +3,87 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <div className="columns">
-        <div className="column is-half is-flex is-flex-direction-column">
-          <h2
-            className="has-text-centered is-size-1"
-            style={{
-              marginTop: "2rem",
-              marginBottom: "1rem",
-              fontWeight: "boldest",
-            }}
-          >
-            WHAT WE DO{" "}
-          </h2>
-          <div className="is-flex is-justify-content-center" style={{}}>
-            <div
-              className="justify-content-center"
+      <div
+        style={{
+          height: "fit-content",
+          marginBottom: "2.5rem",
+          marginTop: "6rem",
+        }}
+      >
+        <div className="columns">
+          <div className="column is-half is-flex is-flex-direction-column">
+            <h2
+              className="has-text-centered is-size-1"
               style={{
-                background: "linear-gradient(to right, #972EFF , #E197FD)",
-                height: ".18rem",
-                width: "9%",
+                marginTop: "2rem",
                 marginBottom: "1rem",
-              }}
-            ></div>
-          </div>
-          <div className="is-flex is-justify-content-center">
-            <p
-              className="has-text-centered is-size-4"
-              style={{
-                maxWidth: "40rem",
-                marginBottom: "3rem",
-                fontWeight: "bold",
+                fontWeight: "boldest",
               }}
             >
-              Let Digifie help you with all your digital needs. There are
-              multiple ways to contact us, choose which one is best for you.
-              Lets start building your dream website.{" "}
-            </p>
+              WHAT WE DO{" "}
+            </h2>
+            <div className="is-flex is-justify-content-center" style={{}}>
+              <div
+                className="justify-content-center"
+                style={{
+                  background: "linear-gradient(to right, #972EFF , #E197FD)",
+                  height: ".18rem",
+                  width: "9%",
+                  marginBottom: "1rem",
+                }}
+              ></div>
+            </div>
+            <div className="is-flex is-justify-content-center">
+              <p
+                className="has-text-centered is-size-4"
+                style={{
+                  maxWidth: "40rem",
+                  marginBottom: "3rem",
+                  fontWeight: "bold",
+                }}
+              >
+                Let Digifie help you with all your digital needs. There are
+                multiple ways to contact us, choose which one is best for you.
+                Lets start building your dream website.{" "}
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="column is-half is-flex is-flex-direction-column">
-          <h2
-            className="has-text-centered is-size-1"
-            style={{
-              marginTop: "2rem",
-              marginBottom: "1rem",
-              fontWeight: "boldest",
-            }}
-          >
-            HOW WE WORK{" "}
-          </h2>
-          <div className="is-flex is-justify-content-center" style={{}}>
-            <div
-              className="justify-content-center"
+          <div className="column is-half is-flex is-flex-direction-column">
+            <h2
+              className="has-text-centered is-size-1"
               style={{
-                background: "linear-gradient(to right, #972EFF , #E197FD)",
-                height: ".18rem",
-                width: "9%",
+                marginTop: "2rem",
                 marginBottom: "1rem",
-              }}
-            ></div>
-          </div>
-          <div className="is-flex is-justify-content-center">
-            <p
-              className="has-text-centered is-size-4"
-              style={{
-                maxWidth: "40rem",
-                marginBottom: "3rem",
-                fontWeight: "bold",
+                fontWeight: "boldest",
               }}
             >
-              Let Digifie help you with all your digital needs. There are
-              multiple ways to contact us, choose which one is best for you.
-              Lets start building your dream website.{" "}
-            </p>
+              HOW WE WORK{" "}
+            </h2>
+            <div className="is-flex is-justify-content-center" style={{}}>
+              <div
+                className="justify-content-center"
+                style={{
+                  background: "linear-gradient(to right, #972EFF , #E197FD)",
+                  height: ".18rem",
+                  width: "9%",
+                  marginBottom: "1rem",
+                }}
+              ></div>
+            </div>
+            <div className="is-flex is-justify-content-center">
+              <p
+                className="has-text-centered is-size-4"
+                style={{
+                  maxWidth: "40rem",
+                  marginBottom: "3rem",
+                  fontWeight: "bold",
+                }}
+              >
+                Let Digifie help you with all your digital needs. There are
+                multiple ways to contact us, choose which one is best for you.
+                Lets start building your dream website.{" "}
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -83,7 +91,7 @@ const About = () => {
       <div
         style={{
           background: "#FCFCFC",
-          height: "40rem",
+          height: "fit-content",
           marginBottom: "2.5rem",
           marginTop: "2.5rem",
         }}
@@ -109,25 +117,103 @@ const About = () => {
             }}
           ></div>
         </div>
-        <div className="columns">
-          <div className="column box is-one-third is-flex" style={{ marginRight: "1rem" }}>
-            <div className="">
-              <h2 className="has-text-centered is-size-2" style={{ paddingBottom: ".7rem" }}>Trust</h2>
-              <p className="has-text-left is-size-5" style={{ paddingBottom: ".7rem", paddingLeft: "1rem", paddingRight: "1rem" }}>
-                We focus on regular communication and transparency from the
-                first day of cooperation. You will get full access to Jira and
-                Repo, regular reports, and invitations to all the meetings.
-              </p>
+        <div style={{ marginLeft: "1rem", marginRight: "1rem" }}>
+          <div className="columns">
+            <div
+              className="column box is-half is-flex box-trust"
+              style={{ marginRight: ".5rem" }}
+            >
+              <div className="">
+                <h2
+                  className="has-text-centered is-size-2"
+                  style={{ paddingBottom: ".7rem" }}
+                >
+                  Trust
+                </h2>
+                <p
+                  className="has-text-left is-size-5"
+                  style={{
+                    paddingBottom: ".7rem",
+                    paddingLeft: "1rem",
+                    paddingRight: "1rem",
+                  }}
+                >
+                  We focus on regular communication and transparency from the
+                  first day of cooperation. You will get full access to Jira and
+                  Repo, regular reports, and invitations to all the meetings.
+                </p>
+              </div>
+            </div>
+            <div className="column box is-half is-flex box-quality" style={{}}>
+              <div className="">
+                <h2
+                  className="has-text-centered is-size-2"
+                  style={{ paddingBottom: ".7rem" }}
+                >
+                  Quality
+                </h2>
+                <p
+                  className="has-text-left is-size-5"
+                  style={{
+                    paddingBottom: ".7rem",
+                    paddingLeft: "1rem",
+                    paddingRight: "1rem",
+                  }}
+                >
+                  We focus on regular communication and transparency from the
+                  first day of cooperation. You will get full access to Jira and
+                  Repo, regular reports, and invitations to all the meetings.
+                </p>
+              </div>
             </div>
           </div>
-          <div className="column box is-one-third is-flex" style={{}}>
-            <div className="">
-              <h2 className="has-text-centered is-size-2" style={{ paddingBottom: ".7rem" }}>Quality</h2>
-              <p className="has-text-left is-size-5" style={{ paddingBottom: ".7rem", paddingLeft: "1rem", paddingRight: "1rem" }}>
-                We focus on regular communication and transparency from the
-                first day of cooperation. You will get full access to Jira and
-                Repo, regular reports, and invitations to all the meetings.
-              </p>
+          <div className="columns">
+            <div
+              className="column box is-half is-flex box-trust"
+              style={{ marginRight: ".5rem" }}
+            >
+              <div className="">
+                <h2
+                  className="has-text-centered is-size-2"
+                  style={{ paddingBottom: ".7rem" }}
+                >
+                  Trust
+                </h2>
+                <p
+                  className="has-text-left is-size-5"
+                  style={{
+                    paddingBottom: ".7rem",
+                    paddingLeft: "1rem",
+                    paddingRight: "1rem",
+                  }}
+                >
+                  We focus on regular communication and transparency from the
+                  first day of cooperation. You will get full access to Jira and
+                  Repo, regular reports, and invitations to all the meetings.
+                </p>
+              </div>
+            </div>
+            <div className="column box is-half is-flex box-quality" style={{}}>
+              <div className="">
+                <h2
+                  className="has-text-centered is-size-2"
+                  style={{ paddingBottom: ".7rem" }}
+                >
+                  Quality
+                </h2>
+                <p
+                  className="has-text-left is-size-5"
+                  style={{
+                    paddingBottom: ".7rem",
+                    paddingLeft: "1rem",
+                    paddingRight: "1rem",
+                  }}
+                >
+                  We focus on regular communication and transparency from the
+                  first day of cooperation. You will get full access to Jira and
+                  Repo, regular reports, and invitations to all the meetings.
+                </p>
+              </div>
             </div>
           </div>
         </div>
