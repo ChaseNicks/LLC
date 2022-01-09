29fa181781bc804bf7de13f1c53b7f1bac51e85d
Leland Hayes <lelandkhayes13@gmail.com>
2022-01-09 17:17:32 -0600

    added 2nd section to service page, with styling

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -213,8 +213,178 @@ const Services = () => {
                 </p>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
 
-            
+      <div
+        className="container"
+        style={{
+          maxWidth: "83rem",
+          marginBottom: "5.7rem",
+          marginTop: "10rem",
+        }}
+      >
+        <div
+          className="columns"
+          style={{ paddingLeft: "1rem", paddingRight: "1rem" }}
+        >
+
+          <div
+            className="column is-half"
+            style={{ marginLeft: "1rem", marginTop: "4rem" }}
+          >
+            {/* 1st row of columns  */}
+            <div className="columns">
+              <div
+                className="column srv-tiles2 is-one-third is-flex is-flex-direction-column is-justify-content-center is-align-items-center grow"
+                style={{
+                  marginLeft: ".5rem",
+                  marginRight: ".5rem",
+                  border: "1.2px solid #3C5CCF",
+                }}
+              >
+                <img src={comm} alt="developer" style={{ height: "4rem", width: "4rem"}} />
+                <p
+                  className={"tile-p  is-size-5"}
+                  style={{
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Empathize
+                </p>
+              </div>
+              <div
+                className="column srv-tiles2 is-one-third is-flex is-flex-direction-column is-justify-content-center is-align-items-center grow"
+                style={{
+                  marginLeft: ".5rem",
+                  marginRight: ".5rem",
+                  border: "1.2px solid #3C5CCF",
+                }}
+              >
+                <img src={define} alt="developer" style={{ height: "4rem", width: "4rem"}} />
+                <p
+                  className={"tile-p  is-size-5"}
+                  style={{
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Define
+                </p>
+              </div>
+              <div
+                className="column srv-tiles2 is-one-third is-flex is-flex-direction-column is-justify-content-center is-align-items-center grow"
+                style={{
+                  marginLeft: ".5rem",
+                  marginRight: ".5rem",
+                  border: "1.2px solid #3C5CCF",
+                }}
+              >
+                <img src={brain} alt="developer" style={{ height: "4rem", width: "4rem"}} />
+                <p
+                  className={"tile-p  is-size-5"}
+                  style={{
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Ideate
+                </p>
+              </div>
+            </div>
+
+            {/* 2ndrow of columns*/}
+            <div className="columns"  style={{ marginTop: "1.5rem" }}>
+              <div
+                className="column srv-tiles2 is-one-third is-flex is-flex-direction-column is-justify-content-center is-align-items-center grow"
+                style={{
+                  marginLeft: ".5rem",
+                  marginRight: ".5rem",
+                  border: "1.2px solid #3C5CCF",
+                }}
+              >
+                <img src={maintain} alt="developer" style={{ height: "4rem", width: "4rem" }} />
+                <p
+                  className={"tile-p is-size-5"}
+                  style={{
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Maintain
+                </p>
+              </div>
+              <div
+                className="column srv-tiles2 is-one-third is-flex is-flex-direction-column is-justify-content-center is-align-items-center grow"
+                style={{
+                  marginLeft: ".5rem",
+                  marginRight: ".5rem",
+                  border: "1.2px solid #3C5CCF",
+                }}
+              >
+                <img src={test} alt="developer" style={{ height: "4rem", width: "4rem" }} />
+                <p
+                  className={"tile-p is-size-5"}
+                  style={{
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Test
+                </p>
+              </div>
+              <div
+                className="column srv-tiles2 is-one-third is-flex is-flex-direction-column is-justify-content-center is-align-items-center grow"
+                style={{
+                  marginLeft: ".5rem",
+                  marginRight: ".5rem",
+                  border: "1.2px solid #3C5CCF",
+                }}
+              >
+                <img src={proto} alt="developer" style={{ height: "4rem", width: "4rem" }} />
+                <p
+                  className={"tile-p  is-size-5"}
+                  style={{
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Prototype
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="column is-flex is-half">
+            <div className="has-text-left">
+              <h1
+                className="is-size-1 is-size-3-mobile has-text-weight-bold"
+                style={{
+                  textAlign: "left",
+
+                  paddingBottom: ".9rem",
+                }}
+              >
+                Existing <br /> Site <span style={{ color: "rgb(60, 92, 207)" }}> / </span>{" "}
+                Revamp
+              </h1>
+              <p
+                className="is-size-5 is-size-6-mobile has-text-weight-semibold"
+                style={{
+                  lineHeight: "2",
+                  letterSpacing: ".8px",
+                  paddingRight: "2rem",
+                }}
+              >
+                {" "}
+                {/* Needs to be changed */}
+                Team up with superstar product designers, frontend/backend
+                developers, cloud architects, and DevOps. 170+ professionals.
+                140+ delivered projects. Full-cycle custom software development
+                services powered by 9 years of best practices.
+              </p>
+            </div>
           </div>
         </div>
       </div>
