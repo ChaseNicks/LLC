f31fe4a9ceda575b456f0ff571b1f22b1ad14750
Leland Hayes <lelandkhayes13@gmail.com>
2022-01-09 11:10:46 -0600

    start build of service page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -5,19 +5,265 @@ const Services = () => {
   return (
     <div>
       <div className="is-flex is-justify-content-center">
-        <h1
-          className="is-size-2 is-size-4-mobile has-text-centered"
-          style={{
-            padding: ".5rem",
-            marginTop: "5rem",
-            maxWidth: "48rem",
-            fontWeight: "900",
-          }}
+        <div className="is-align-items-center has-text-centered">
+          <h1
+            className="is-size-2 is-size-3-mobile"
+            style={{
+              padding: ".5rem",
+              marginTop: "5rem",
+              maxWidth: "48rem",
+              fontWeight: "900",
+            }}
+          >
+            {" "}
+            {/* Needs to be changed */}
+            Custom software development services from professionals you’ll enjoy
+            working with
+          </h1>
+          <p
+            className="is-size-6 is-size-6-mobile"
+            style={{
+              padding: ".5rem",
+              marginTop: ".7rem",
+              maxWidth: "48rem",
+              fontWeight: "900",
+              lineHeight: "2",
+            }}
+          >
+            {" "}
+            {/* Needs to be changed */}
+            Team up with superstar product designers, frontend/backend
+            developers, cloud architects, and DevOps. 170+ professionals. 140+
+            delivered projects. Full-cycle custom software development services
+            powered by 9 years of best practices.
+          </p>
+        </div>
+      </div>
+
+      <div
+        className="container"
+        style={{
+          maxWidth: "83rem",
+          marginBottom: "5.7rem",
+          marginTop: "5.7rem",
+        }}
+      >
+        <div
+          className="columns"
+          style={{ paddingLeft: "1rem", paddingRight: "1rem" }}
         >
-          {" "}
-          Custom software development services from
-          professionals you’ll enjoy working with
-        </h1>
+          <div className="column is-flex is-half">
+            <div className="has-text-left">
+              <h1
+                className="is-size-1 is-size-3-mobile has-text-weight-bold"
+                style={{
+                  textAlign: "left",
+
+                  paddingBottom: ".9rem",
+                }}
+              >
+                Custom <br /> Web <span style={{ color: "#ff934f" }}> / </span>{" "}
+                Mobile Site
+              </h1>
+              <p
+                className="is-size-5 is-size-6-mobile has-text-weight-semibold"
+                style={{
+                  lineHeight: "2",
+                  letterSpacing: ".8px",
+                  paddingRight: "2rem",
+                }}
+              >
+                {" "}
+                {/* Needs to be changed */}
+                Team up with superstar product designers, frontend/backend
+                developers, cloud architects, and DevOps. 170+ professionals.
+                140+ delivered projects. Full-cycle custom software development
+                services powered by 9 years of best practices.
+              </p>
+            </div>
+          </div>
+
+          <div
+            className="column is-half"
+            style={{ marginLeft: "1rem", marginTop: "3rem" }}
+          >
+            {/* 1st row of columns*/}
+            <div className="columns">
+              <div
+                className="column is-one-third is-flex is-flex-direction-column is-justify-content-center is-align-items-center grow"
+                style={{
+                  marginLeft: ".5rem",
+                  marginRight: ".5rem",
+                  border: "1.2px solid #ff934f",
+                }}
+              >
+                <h2
+                  className="is-size-3 has-text-centered"
+                  style={{
+                    paddingBottom: ".7rem",
+                    // marginLeft: "1rem",
+                    // marginRight: "1rem",
+                  }}
+                >
+                  SVG
+                </h2>
+                <p
+                  className={""}
+                  style={{
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Maintain
+                </p>
+              </div>
+              <div
+                className="column is-one-third is-flex is-flex-direction-column is-justify-content-center is-align-items-center grow"
+                style={{
+                  marginLeft: ".5rem",
+                  marginRight: ".5rem",
+                  border: "1.2px solid #ff934f",
+                }}
+              >
+                <h2
+                  className="is-size-3 has-text-centered"
+                  style={{
+                    paddingBottom: ".7rem",
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  SVG
+                </h2>
+                <p
+                  className={""}
+                  style={{
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Test
+                </p>
+              </div>
+              <div
+                className="column is-one-third is-flex is-flex-direction-column is-justify-content-center is-align-items-center grow"
+                style={{
+                  marginLeft: ".5rem",
+                  marginRight: ".5rem",
+                  border: "1.2px solid #ff934f",
+                }}
+              >
+                <h2
+                  className="is-size-3 has-text-centered"
+                  style={{
+                    paddingBottom: ".7rem",
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  SVG
+                </h2>
+                <p
+                  className={""}
+                  style={{
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Prototype
+                </p>
+              </div>
+            </div>
+
+            {/* 2nd row of columns  */}
+            <div className="columns" style={{ marginTop: "1.5rem" }}>
+              <div
+                className="column is-one-third is-flex is-flex-direction-column is-justify-content-center is-align-items-center grow"
+                style={{
+                  marginLeft: ".5rem",
+                  marginRight: ".5rem",
+                  border: "1.2px solid #ff934f",
+                }}
+              >
+                <h2
+                  className="is-size-3 has-text-centered"
+                  style={{
+                    paddingBottom: ".7rem",
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  SVG
+                </h2>
+                <p
+                  className={""}
+                  style={{
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Maintain
+                </p>
+              </div>
+              <div
+                className="column is-one-third is-flex is-flex-direction-column is-justify-content-center is-align-items-center grow"
+                style={{
+                  marginLeft: ".5rem",
+                  marginRight: ".5rem",
+                  border: "1.2px solid #ff934f",
+                }}
+              >
+                <h2
+                  className="is-size-3 has-text-centered"
+                  style={{
+                    paddingBottom: ".7rem",
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  SVG
+                </h2>
+                <p
+                  className={""}
+                  style={{
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Test
+                </p>
+              </div>
+              <div
+                className="column is-one-third is-flex is-flex-direction-column is-justify-content-center is-align-items-center grow"
+                style={{
+                  marginLeft: ".5rem",
+                  marginRight: ".5rem",
+                  border: "1.2px solid #ff934f",
+                }}
+              >
+                <h2
+                  className="is-size-3 has-text-centered"
+                  style={{
+                    paddingBottom: ".7rem",
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  SVG
+                </h2>
+                <p
+                  className={""}
+                  style={{
+                    marginLeft: "1rem",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Prototype
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
